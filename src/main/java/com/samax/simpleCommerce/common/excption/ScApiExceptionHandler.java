3bfcb7ce3b2e3f518755434ec015ee20353885c0
samax-dx <samax.dx@aol.com>
2024-02-04 00:39:14 +0600

    added unknowe exception handler (issue #4)

--- src/main/java/com/samax/simpleCommerce/common/excption/ScApiExceptionHandler.java
+++ src/main/java/com/samax/simpleCommerce/common/excption/ScApiExceptionHandler.java
@@ -1,13 +1,24 @@
 package com.samax.simpleCommerce.common.excption;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.PrintStream;
+
 
 @ControllerAdvice
 public class ScApiExceptionHandler {
+
+    private final PrintStream errorOutputStream;
+
+
+    public ScApiExceptionHandler(@Autowired(required = false) PrintStream errorOutputStream) {
+        this.errorOutputStream = errorOutputStream == null ? System.err : errorOutputStream;
+    }
 
     @ExceptionHandler(ScHttpException.class)
     public ResponseEntity<?> handleClientException(ScHttpException exception) {
@@ -21,6 +32,12 @@ public class ScApiExceptionHandler {
         Object[] messageArguments = exception.getDetailMessageArguments();
         String validationErrorMessage = messageArguments != null && messageArguments.length > 1 ? (String) messageArguments[1] : "invalid input";
         return ResponseEntity.status(exception.getStatusCode()).body(validationErrorMessage);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception exception) {
+        exception.printStackTrace(errorOutputStream);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("unknown error");
     }
 
 }
